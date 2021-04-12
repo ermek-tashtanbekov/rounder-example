7cd7a3694aca6af4026da3758507f7e7809c797c
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-04-12 21:45:48 +0600

    added Route in components Home About Contacts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import { Route } from 'react-router';
 import './App.css';
+import About from './componenet/About/About';
+import Contacts from './componenet/Contacts/Contacts';
+import Home from './componenet/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Route path="/about" component={About} />
+     <Route path="/contacts" component={Contacts} />
+     <Route path="/" component={Home} />
     </div>
   );
 }
