3c3d4a0c72224d16b81b56429269d84c891a76f5
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-04-12 22:19:46 +0600

    added new teg NavLink and activeStyle and activeClassName

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { Route, Switch } from 'react-router';
+import { Link, NavLink } from 'react-router-dom';
 import './App.css';
 import About from './componenet/About/About';
 import Contacts from './componenet/Contacts/Contacts';
@@ -7,14 +8,24 @@ import Home from './componenet/Home/Home';
 import NotFound from './componenet/NotFound/NotFound';
 
 function App() {
+  const activeLInk ={
+    fontWeight : "bold",
+  }
   return (
-
+  
     <div className="App">
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contacts">Contacts</a></li>
+        <li><NavLink activeStyle={activeLInk} activeClassName="activeLInk" exact to="/">Home</NavLink></li>
+        <li><NavLink activeStyle={activeLInk} activeClassName="activeLInk" to="/about">About</NavLink></li>
+        <li><NavLink activeStyle={activeLInk} activeClassName="activeLInk" to="/contacts">Contacts</NavLink></li>
       </ul>
+
+
+      {/* <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/contacts">Contacts</Link></li>
+      </ul> */}
 
       <Switch>
         <Route path="/about" component={About} />
