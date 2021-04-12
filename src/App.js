dc64af2eb8a8194b7f7f56581b13f0ed521126e5
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-04-12 22:02:41 +0600

    added ul and li a teg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import About from './componenet/About/About';
 import Contacts from './componenet/Contacts/Contacts';
 import Home from './componenet/Home/Home';
+import NotFound from './componenet/NotFound/NotFound';
 
 function App() {
   return (
+
     <div className="App">
-     <Route path="/about" component={About} />
-     <Route path="/contacts" component={Contacts} />
-     <Route path="/" component={Home} />
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contacts">Contacts</a></li>
+      </ul>
+
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/contacts" component={Contacts} />
+        <Route path="/" component={Home} />
+        <Route path="/" component={NotFound} />
+      </Switch>
+
+
+
+
+
+      {/* <Route path="/" component={Home} /> Переадресатция Слок*/}
     </div>
   );
 }
